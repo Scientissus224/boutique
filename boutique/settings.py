2959fbaea5a6cf7bdce4e9f10d4238a0e8280326
ScientyDev224 <warabakabagui@gmail.com>
2025-04-12 00:00:32 +0000

    Migration vers dj_database_url pour la connexion PostgreSQL

--- boutique/settings.py
+++ boutique/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -94,14 +95,11 @@ TEMPLATES = [
 WSGI_APPLICATION = 'boutique.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME', 'waraba_guinee'),
-        'USER': os.getenv('DB_USER', 'waraba_guinee_user'),
-        'PASSWORD': os.getenv('bsqpIXTODz1gtKX00aHj3PyqbcW7fvH1'),
-        'HOST': os.getenv('DB_HOST', 'localhost'),
-        'PORT': os.getenv('DB_PORT', '5432'),
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL', 'postgres://waraba_guinee_user:bsqpIXTODz1gtKX00aHj3PyqbcW7fvH1@dpg-cvspkd24d50c73d5ovh0-a.oregon-postgres.render.com/waraba_guinee'),
+        conn_max_age=600,
+        ssl_require=True  # sécurise la connexion
+    )
 }
 
 # Validation du mot de passe
