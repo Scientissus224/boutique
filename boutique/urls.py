8e6780677a1a86728a9089c208227d4f6b61f29e
ScientyDev224 <warabakabagui@gmail.com>
2025-04-14 13:00:27 +0000

    fichier static configuration

--- boutique/urls.py
+++ boutique/urls.py
@@ -152,6 +152,8 @@ urlpatterns = [
 handler404 = 'shop.views.custom_404'
 
 
-# Configuration pour servir les fichiers médias en mode DEBUG
+# Configuration pour servir les fichiers statiques et médias en mode DEBUG
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
